dc52e84931c4ea892d10e7bc58c95b7d00103290
davidmitesh <excelrock_mitesh@yahoo.com>
2018-05-04 08:13:07 +0545

    making new projects page and deploying to heroku

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use((req,res,next)=>{
   var log=now+`${req.method} ${req.url}`;
   fs.appendFile('server.log',log+'\n',(err)=>{
     console.log(err);
-  })
-  console.log(log);
-  res.render('maintenance.hbs');
-})
+  });
+//   console.log(log);
+//   res.render('maintenance.hbs');
+next();
+ })
 app.use(express.static(__dirname+'/public'));
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('capitalize',(text)=>{
@@ -32,6 +33,12 @@ app.get('/',(req,res)=>{
 app.get('/home',(req,res)=>{
   res.render('home.hbs',{
     title: "mitesh",
+    year: new Date().getFullYear()
+  });
+});
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    title:"mitesh",
     year: new Date().getFullYear()
   });
 });
